507cdd9c7e11d42695c5bed5459c44114aa28b04
jothbc <jothbc@gmail.com>
2020-06-06 12:50:40 -0300

    controllers created

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,23 +1,9 @@
 import { Router } from 'express';
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
-import UserRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
+const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', async (request, response) => {
-  const userRepository = new UserRepository();
-  const { email, password } = request.body;
-
-  const authenticateUser = new AuthenticateUserService(userRepository);
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json({ user, token });
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;
